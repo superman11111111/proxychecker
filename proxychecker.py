61398db1b296f7206c978aab5622196d6c6e9b2c
mkyper <mrckyper@gmail.com>
2022-01-05 14:57:51 +0100

    add anonymity checks

--- proxychecker.py
+++ proxychecker.py
@@ -9,104 +9,192 @@ import time
 import json
 import os
 
-socket.setdefaulttimeout(30)
+# Settings
+PUBLIC_IP = ""  # IP of the server running this script, I am not automating this for security reasons
+PROXY_CHECK_URL = "https://www.myip.com/"
+TIMEOUT = 30
+
+# Global variables
+ANON_CHECK_URL = ""
+NOT_ANON_FLAG = "not_anon"
+ANON_FLAG = "anon"
 CHECKING_STARTED = False
 CHECKING_THREAD = None
+ANONCHECKING_THREAD = None
+
+socket.setdefaulttimeout(TIMEOUT)
 
 
-def check_proxy(pip, out_queue: Queue):
+def get(url, proxy_data=dict()):
     try:
-        proxy_handler = urllib.request.ProxyHandler({"https": pip})
+        proxy_handler = urllib.request.ProxyHandler(proxy_data)
         opener = urllib.request.build_opener(proxy_handler)
         opener.addheaders = [("User-agent", "Mozilla/5.0")]
-        urllib.request.install_opener(opener)
-        start = time.time()
-        sock = urllib.request.urlopen("https://www.myip.com/")
-        out_queue.put((time.time() - start, pip))
-    except urllib.error.HTTPError as e:
-        return e
-    except Exception as detail:
-        return detail
-    return 0
-
-
-def consume_queue(proxies: list, q: Queue, stop_event: Event):
-    working = []
-    import time
-
-    l = len(proxies)
-    i = 0
-    while True:
-        # print(stop_event.is_set())
-        if stop_event.is_set():
-            break
-        if not q.empty():
-            t, pip = q.get(block=False)
-            print(f"\r{i}/{l} {t} {pip}  ", end="")
-            working.append((t, pip))
-            i += 1
-        time.sleep(0.01)
-    import json
-
-    print()
-    print("Saving Proxies")
-    sort = sorted(working, key=lambda x: x[0])
-    f = open("working.json", "w")
-    f.write(json.dumps(sort))
-    f.close()
-
-
-# Example run : echo -ne "192.168.1.1:231\n192.168.1.2:231" | python proxy_checkpy3-async.py
-# proxies = sys.stdin.readlines()
-# proxies = input("[proxies]\n").split('\n')
+        return opener.open(url)
+    except Exception:
+        pass
+    return None
 
 
 def start_checking(proxy_file="proxies.txt"):
     time.sleep(2)
+    if not os.path.isfile(proxy_file):
+        print(f"[Error] Proxy file missing: {proxy_file}")
+        time.sleep(1)
+        global CHECKING_STARTED
+        CHECKING_STARTED = False
+        return
     print()
-    q = Queue()
+    qu = Queue()
     stop_event = Event()
+    n_proxies = 0
+
+    def check_proxy(pip):
+        global PROXY_CHECK_URL
+        start = time.time()
+        if get(PROXY_CHECK_URL, {"https": pip}):
+            qu.put((time.time() - start, pip))
+
+    def consume_queue():
+        working = []
+        i = 0
+        while True:
+            if stop_event.is_set():
+                break
+            if not qu.empty():
+                t, pip = qu.get(block=False)
+                print(f"\r{i}/{n_proxies} {t} {pip}  ", end="")
+                working.append((t, pip, NOT_ANON_FLAG))
+                i += 1
+            time.sleep(0.01)
+        print()
+        print("Saving Proxies to working_info.json")
+        working.sort(key=lambda x: x[0])
+        working_json = json.dumps(working)
+        f = open("working_info.json", "w")
+        f.write(working_json)
+        f.close()
+
     while True:
         proxies = open(proxy_file, "r").read().split("\n")
+        n_proxies = len(proxies)
         threads = []
-        consume_thread = Thread(target=consume_queue, args=(proxies, q, stop_event,))
+        consume_thread = Thread(target=consume_queue)
         consume_thread.daemon = False
         consume_thread.start()
-
         for proxy in proxies:
-            thread = Thread(target=check_proxy, args=(proxy.strip(), q,))
+            thread = Thread(target=check_proxy, args=(proxy.strip(),))
             thread.daemon = False
-            thread.start()
             threads.append(thread)
-
-        for thread in threads:
-            thread.join()
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
         stop_event.set()
         consume_thread.join()
         stop_event.clear()
-        # print('Waiting 1 minutes')
-        # time.sleep(60)
+
+
+def start_anon_checking():
+    time.sleep(2)
+    while not os.path.isfile("working_info.json"):
+        time.sleep(1)
+    print("Starting anonymity checks")
+    qu = Queue()
+    stop_event = Event()
+    n_proxies = 0
+
+    def check_anon(pip: str):
+        try:
+            # start_t = time.time()
+            global ANON_CHECK_URL
+            req = get(ANON_CHECK_URL, proxies={"http": pip, "https": pip})
+            if isinstance(req, urllib.request._UrlopenRet):
+                text = req.read()
+                if "X-Forwarded-For" in text:
+                    return
+            else:
+                return
+        except Exception:
+            return
+        qu.put(pip)
+
+    def consume_queue():
+        good = []
+        while True:
+            if stop_event.is_set():
+                break
+            if not qu.empty():
+                good.append(qu.get(block=False))
+            time.sleep(0.01)
+        f = open("working_info.json", "r")
+        _r = f.read()
+        f.close()
+        working_proxies = json.loads(_r)
+        working_info = []
+        for tt, proxy, _ in working_proxies:
+            if proxy in good:
+                working_info.append((tt, proxy, "anon"))
+            else:
+                working_info.append((tt, proxy, "not_anon"))
+        working_info.sort(key=lambda x: x[0])
+        print("Writing anonymity infos to file")
+        open("working_info.json", "w").write(json.dumps(working_info))
+
+    while True:
+        working_url = "http://127.0.0.1:4001/api/working?type=all"
+        req = get(working_url)
+        if not req:
+            print(f"No working data from {working_url}, retrying in 1")
+            time.sleep(1)
+            continue
+        _read = req.read()
+        try:
+            proxies = json.loads(_read)
+        except json.JSONDecodeError:
+            # print(_read)
+            continue
+        n_proxies = len(proxies)
+        threads = []
+        for i in range(n_proxies):
+            tt, proxy, type = proxies[i]
+            t = Thread(target=check_anon, args=(proxy,))
+            t.daemon = False
+            threads.append(t)
+        for t in threads:
+            t.start()
+
+        consume_thread = Thread(target=consume_queue, args=())
+        consume_thread.start()
+
+        for t in threads:
+            t.join()
+        stop_event.set()
+        consume_thread.join()
+        stop_event.clear()
 
 
 def toggle_checking():
-    global CHECKING_STARTED, CHECKING_THREAD
+    global CHECKING_STARTED, CHECKING_THREAD, ANONCHECKING_THREAD
     if CHECKING_STARTED:
         CHECKING_THREAD.join()
         CHECKING_THREAD = None
+        ANONCHECKING_THREAD.join()
+        ANONCHECKING_THREAD = None
     else:
         CHECKING_THREAD = Thread(target=start_checking)
         CHECKING_THREAD.start()
+        ANONCHECKING_THREAD = Thread(target=start_anon_checking)
+        ANONCHECKING_THREAD.start()
     CHECKING_STARTED = not CHECKING_STARTED
     return CHECKING_STARTED
 
 
 def server(host: str, port: int):
     from flask import Flask, jsonify, request, abort, redirect
-    import logging
 
-    log = logging.getLogger("werkzeug")
-    log.setLevel(logging.ERROR)
-
+    cli = sys.modules["flask.cli"]
+    cli.show_server_banner = lambda *x: None
     app = Flask(__name__)
 
     @app.route("/", methods=["GET", "POST"])
@@ -128,26 +216,52 @@ def server(host: str, port: int):
                 n = int(n)
             except:
                 return "Wrong parameter n (use int)"
-        if not os.path.isfile("working.json"):
-            return "Proxy check still running, Try again later"
-        f = open("working.json", "r")
+        if not os.path.isfile("working_info.json"):
+            return "No data exists, Retry later"
+
+        types = ["all", "anon"]
+        type = request.args.get("type")
+
+        if type not in types:
+            return f"Invalid type, please use type from {types}"
+
+        f = open("working_info.json", "r")
         working_json = json.loads(f.read())
         f.close()
+        if type == "anon":
+            working_json = [x for x in working_json if x[2] == "anon"]
         if n:
             return jsonify(working_json[:n])
         return jsonify(working_json)
+
+    @app.route("/api/working/")
+    def red_working():
+        return redirect("/api/working")
 
     @app.route("/api/headers")
     def hedaers():
         return str(request.headers)
 
-    print(f" * Running Flask on http://{host}:{port}")
+    @app.route("/api/headers/")
+    def red_headers():
+        return redirect("/api/headers")
+
+    # print(f" * Running Flask on http://{host}:{port}")
+    import logging
+
+    logging.basicConfig(filename="flask.log", level=logging.DEBUG)
+
+    def console():
+        time.sleep(2)
+        print(open("flask.log", "r").read().split("\n")[-2], flush=True)
+
+    Thread(target=console).start()
     app.run(host, port)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-ho", "--host", type=str, help="Hostname", default="127.0.0.1")
-parser.add_argument("-p", "--port", type=int, help="Port", default=4001)
-
+parser.add_argument("-ho", "--host", type=str, help="Hostname", default="0.0.0.0")
+parser.add_argument("-p", "--port", type=int, help="Port", default=4000)
 args = parser.parse_args()
+ANON_CHECK_URL = f"http://{PUBLIC_IP}:{args.port}/api/headers/"
 server(args.host, args.port)
